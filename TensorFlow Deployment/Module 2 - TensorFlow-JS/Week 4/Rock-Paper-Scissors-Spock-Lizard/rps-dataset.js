544d0a9e2aa895483e2f227dd268cfd9a12fd615
unknown <777JonathanSum@gmail.com>
2019-10-15 12:04:15 -0700

    Signed-off-by: unknown <777JonathanSum@gmail.com>

---
+++ TensorFlow Deployment/Module 2 - TensorFlow-JS/Week 4/Rock-Paper-Scissors-Spock-Lizard/rps-dataset.js
@@ -0,0 +1,35 @@
+class RPSDataset {
+  constructor() {
+    this.labels = []
+  }
+
+  addExample(example, label) {
+    if (this.xs == null) {
+      this.xs = tf.keep(example);
+      this.labels.push(label);
+    } else {
+      const oldX = this.xs;
+      this.xs = tf.keep(oldX.concat(example, 0));
+      this.labels.push(label);
+      oldX.dispose();
+    }
+  }
+  
+  encodeLabels(numClasses) {
+    for (var i = 0; i < this.labels.length; i++) {
+      if (this.ys == null) {
+        this.ys = tf.keep(tf.tidy(
+            () => {return tf.oneHot(
+                tf.tensor1d([this.labels[i]]).toInt(), numClasses)}));
+      } else {
+        const y = tf.tidy(
+            () => {return tf.oneHot(
+                tf.tensor1d([this.labels[i]]).toInt(), numClasses)});
+        const oldY = this.ys;
+        this.ys = tf.keep(oldY.concat(y, 0));
+        oldY.dispose();
+        y.dispose();
+      }
+    }
+  }
+}
